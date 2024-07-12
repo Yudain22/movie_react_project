bea54c8b4d98c0211272cbdd6bc218b203507c41
Yudain22 <di4046@gmail.com>
2024-07-12 15:34:48 +0900

    card 100자 제한, 클릭시 상세페이지

--- movieapp/src/components/MovieCard/MovieCard.jsx
+++ movieapp/src/components/MovieCard/MovieCard.jsx
@@ -4,8 +4,10 @@ import Star from '../../assets/star.png';
 
 export default function MovieCard({movie}) {
 	return (
-		<a className='movie_card'>
+		<a className='movie_card'href={`https://www.themoviedb.org/movie/${movie.id}?language=ko`} 
+		target='_blank'>
 			<img
+				
 				src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
 				alt='movie poster'
 				className='movie_poster'
@@ -20,7 +22,7 @@ export default function MovieCard({movie}) {
 						<img src={Star} alt='rating icon' className='card_emoji' />
 					</p>
 				</div>
-				<p className='movie_description'>{movie.overview}</p>
+				<p className='movie_description'>{movie.overview.slice(0, 100) + "..."}</p>
 			</div>
 		</a>
 	);
